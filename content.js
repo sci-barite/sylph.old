aa293f2c987d9d0ef6656cdf95fa7b332f9575c6
sci-barite <romo.mulas@gmail.com>
2022-10-02 00:03:39 +0300

    Sylph - 1.3 Efreet

--- content.js
+++ content.js
@@ -5,6 +5,7 @@ var SKILLS = 'NA';
 var ENGLISH = 'NA';
 var LOCATION = '';
 var POSITION = 'Angular';
+var RESPONSE = 0;
 
 function SiftDjinni() {
     NAME = document.querySelector("#candidate_name")?.innerText;
@@ -67,23 +68,38 @@ function SiftLinked() {
     LINK = document.URL;
 }
 
+function SylphBack(response) {
+    if (JSON.stringify(response).length > 10) {
+        RESPONSE++
+        var DUP = "✅ ";
+        if (RESPONSE % 2 == 0) {
+            let message = JSON.stringify(response);
+            console.log(message);
+            if (message.includes("DUPLICATE")) DUP = "⚠️ DUPLICATE! "
+            alert(DUP+NAME+"\nPosition: "+POSITION+"\nSkills: "+SKILLS+"\nEnglish: "+ENGLISH)
+        }
+    }
+}
+
 chrome.runtime.onMessage.addListener((request, sender) => {
     console.log('Request', request);
     if (request.name == 'Sylph') {
         switch (request.site.substring(12,18)) {
             case "linked": SiftLinked(); break;
             case "ni.co/": SiftDjinni(); break;
-            case "upwork": SiftUpwork(); break;
+            case "upwork": SiftUpwork(); break; // The function should check if it's a profile or proposal page!
             default: alert(request.site.substring(12,18)+": Can't read website name!"); return;
         }
         const XSnd = new XMLHttpRequest();
+        XSnd.onreadystatechange = function() {
+            SylphBack(XSnd.response);
+         }
         XSnd.open('GET', // Probably better to replace it with POST at some point, but for now this works well.
         'https://script.google.com/macros/s/AKfycbykyHuIELGCZxsCs-WSK8nr5FfBV6l8PMtF94eN3hWBxoay1lD_s_fL0lGU_yNJPi4e/exec?'+
         'name='+NAME+'&pos='+POSITION // Ideally it should be the bookmark's folder title, but now it's declare in the code.
         +'&skills='+SKILLS+'&eng='+ENGLISH+'&rate='+RATE+'&loc='+LOCATION+'&url='+LINK,
         true);
         XSnd.send();
-        alert("✅ "+NAME+"\nPosition: "+POSITION+"\nSkills: "+SKILLS+"\nEnglish: "+ENGLISH)
     }
 });
 
