c57424a743d6273e81203c0b2734d5a91f8699a8
sci-barite <romo.mulas@gmail.com>
2022-10-02 13:07:04 +0300

    Sylph - 1.4 Marit

--- content.js
+++ content.js
@@ -19,51 +19,27 @@ function SiftDjinni() {
 }
 
 function SiftLinked() {
-    var strings = [];
+    var Sifted = [];
     document.querySelectorAll('[aria-hidden="true"]').forEach(item => {item.removeAttribute('aria-hidden');});
-    for (i=0; i<document.querySelectorAll(".visually-hidden").length; i++) {
-        if (document.querySelectorAll(".visually-hidden")[i]?.innerText.startsWith("Skills")) {
-            for (var y=i; y<(i+12); y++) {
-                strings.push(document.querySelectorAll(".visually-hidden")[y]?.innerText);
-            }
-        }
-        if (document.querySelectorAll(".visually-hidden")[i]?.innerText.startsWith("Languages")) {
-            for (var z=i; z<(i+6); z++) {
-                strings.push(document.querySelectorAll(".visually-hidden")[z]?.innerText);
-            }
-        }
+    
+    // Main Skills first
+    for (i=0; i<3; i++) {
+        Sifted.push(' '+document.querySelectorAll("a[data-field='skill_card_skill_topic']")[i].innerText.split("\n")[0]);
     }
-    var skills = [];
-    strings.forEach(function(entry,j) {
-        if (strings[j]?.startsWith('Skil') || strings[j]?.startsWith('Engl')) {
-            switch (strings[j]?.substring(4,7)) {
-                case 'ls:' :
-                    let subskills = strings[j]?.substring(8).split(" · ");
-                    subskills.forEach(function(entry,k) {
-                        if (skills.indexOf(' '+subskills[k]) == -1) skills.push(' '+subskills[k]);
-                    });
-                    break;
-                case 'ls': 
-                    for (var i=1; i<12; i++) {
-                        if (!parseInt(strings[j+i]?.charAt(0)) && strings[j+i]?.length < 35) {
-                            switch (strings[j+i]?.substring(0,5)) {
-                                case "Skill": case "Passe": case "Endor": case "Compl": case "Schoo": case 'Langu':
-                                case "Unive": case "Recom": case "Recei": case "Given" : case "Nothi": break;
-                                case "Engli": case "Англи": ENGLISH = strings[j+i+1]; i=i+16; break;
-                                case "Influ": case "Group":  case "Inter": i=i+16; break;
-                                case "Proje": i=i+2; break;
-                                default:
-                                    if (skills.indexOf(' '+strings[j+i]) == -1) skills.push(' '+strings[j+i]);
-                                    break;
-                            }
-                        }
-                    }
-                    break;
-                case 'ish' : ENGLISH = strings[j+1]; break;
-            }
-        }
-    });
-    SKILLS = skills.toString().substring(1);
+
+    const hidden = document.querySelectorAll(".visually-hidden");
+    var collated = " · ";
+    for (i=0; i < hidden.length; i++) {
+        if (hidden[i].innerText.includes("Skills:")) collated = collated+hidden[i].innerText.substring(8)+" · ";
+        else if (hidden[i].innerText.includes("English")) ENGLISH = hidden[i+1].innerText;
+    }
+
+    var subskills = collated.split(" · ");
+    for (i=0; i<subskills.length; i++) {
+        if (Sifted.indexOf(' '+subskills[i]) == -1 && Sifted != '') Sifted.push(' '+subskills[i]);
+    }
+
+    SKILLS = Sifted.toString().substring(1);
     NAME = document.title.replace(' | LinkedIn', '');
     LINK = document.URL;
 }
