2bfe2e93b1b097c1d897a3a493a28ab6a5dd0d76
sci-barite <romo.mulas@gmail.com>
2022-10-04 02:08:46 +0300

    FWWA 1.2.1 Lancer

--- webapp.js
+++ webapp.js
@@ -4,37 +4,39 @@
  * Now writing data coming from Upwork, and Djinni, too...
  */
  function doGet(e) {
-    if (e.parameter.url.includes("linkedin")) var DB = SpreadsheetApp.openById(/** "SHEET_ID" */).getSheetByName("DB");
-    else var DB = SpreadsheetApp.openById(/** "SHEET_ID" */).getSheetByName("FreelanceDB");
-    const Today = Utilities.formatDate(new Date(), "GMT+3", "dd/MM/yyyy");
-    const Names = DB.getRange('A:A').getValues();
+  var get = e.parameter;
+  if (get.url.includes("linkedin")) var DB = SpreadsheetApp.openById(/** "SHEET_ID" */).getSheetByName("DB");
+  else var DB = SpreadsheetApp.openById(/** "SHEET_ID" */).getSheetByName("FreelanceDB");
+  const Today = Utilities.formatDate(new Date(), "GMT+3", "dd/MM/yyyy");
+  const Names = DB.getRange('A:A').getValues();
 
-    var JSONString = JSON.stringify([e.parameter.name]);  
-    var JSONOutput = ContentService.createTextOutput(JSONString+' parameter invalid.\n\nHave a nice day!');
-    JSONOutput.setMimeType(ContentService.MimeType.JSON);
+  var JSONString = JSON.stringify([get.name]);  
+  var JSONOutput = ContentService.createTextOutput(JSONString+' parameter invalid.\n\nHave a nice day!');
+  JSONOutput.setMimeType(ContentService.MimeType.JSON);
 
-    if (e.parameter.name == null) return JSONOutput;
+  if (get.name == null) return JSONOutput;
 
-    const Search = (element) => element == e.parameter.name;
-    if (Names.findIndex(Search) != -1) var name = 'DUPLICATE! '+e.parameter.name; else var name = e.parameter.name;
+  const Search = (element) => element == get.name;
+  if (Names.findIndex(Search) != -1) var name = 'DUPLICATE! '+get.name; else var name = get.name;
 
-    DB.appendRow(
-    [name, '', '0.New', 'Sylph', Today, e.parameter.pos, e.parameter.skills, e.parameter.loc, '', e.parameter.more, '', '', e.parameter.eng, e.parameter.rate]);
-    var Name = DB.getRange('A'+DB.getLastRow());
-    var Row = DB.getRange(DB.getLastRow()+':'+DB.getLastRow());
-    var Link = SpreadsheetApp.newRichTextValue()
-      .setText(name)
-      .setLinkUrl(e.parameter.url)
-      .build();
-    Name.setRichTextValue(Link);
-    Name.offset(0,1).insertCheckboxes();
-    Row.offset(-1,0).copyTo(Row, SpreadsheetApp.CopyPasteType.PASTE_FORMAT, false);
-    Name.offset(0,3).setFontWeight("bold");
-    Row.setVerticalAlignment('middle');
+  DB.appendRow([
+    name, '', '0.New', 'Sylph', Today, get.pos, get.skills, get.loc, '', get.more, '', '', get.eng, get.rate
+  ]);
+  var Name = DB.getRange('A'+DB.getLastRow());
+  var Row = DB.getRange(DB.getLastRow()+':'+DB.getLastRow());
+  var Link = SpreadsheetApp.newRichTextValue()
+    .setText(name)
+    .setLinkUrl(get.url)
+    .build();
+  Name.setRichTextValue(Link);
+  Name.offset(0,1).insertCheckboxes();
+  Row.offset(-1,0).copyTo(Row, SpreadsheetApp.CopyPasteType.PASTE_FORMAT, false);
+  Name.offset(0,3).setFontWeight("bold");
+  Row.setVerticalAlignment('middle');
 
-    JSONString = JSON.stringify(Row.getValues());  
-    JSONOutput = ContentService.createTextOutput(JSONString+"🧚‍♀️ Sylph's spell was casted successfully!");
-    JSONOutput.setMimeType(ContentService.MimeType.JSON);
+  JSONString = JSON.stringify(Row.getValues());  
+  JSONOutput = ContentService.createTextOutput(JSONString+"🧚‍♀️ Sylph's spell was casted successfully!");
+  JSONOutput.setMimeType(ContentService.MimeType.JSON);
 
-    return JSONOutput;
+  return JSONOutput;
 }
