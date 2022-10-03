439df293db41084471ba838f7f6d4fcf5dbf2043
sci-barite <romo.mulas@gmail.com>
2022-10-03 15:13:35 +0300

    Sylph 1.5.0 Dao

--- src/background.js
+++ src/background.js
@@ -4,11 +4,10 @@ chrome.runtime.onInstalled.addListener(()=> {
 
 chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
     const parent = bookmark.parentId;
-    chrome.bookmarks.get(parent, (folder) => {
-        const position = folder[0].title;
-        alert(parent+' '+position);
+    chrome.bookmarks.get(parent, (bmt) => {
+        const folder = bmt[0].title;
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-            chrome.tabs.sendMessage(tabs[0].id, { name: 'Sylph', site: bookmark.url, pos: position });
+            chrome.tabs.sendMessage(tabs[0].id, { name: 'Sylph', site: bookmark.url, position: folder });
             console.log('Bookmark created, Sylph is casting her spell...');
             chrome.pageAction.setIcon({
                 tabId: tabs[0].id,
