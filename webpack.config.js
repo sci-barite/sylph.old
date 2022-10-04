00545ace17447938680201432677166b7301834e
sci-barite <romo.mulas@gmail.com>
2022-10-04 13:55:36 +0300

    Sylph - 1.5.5 Dao

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const glob = require("glob");
+const CopyPlugin = require("copy-webpack-plugin"); 
+
+module.exports = {
+    entry: glob.sync('./src/**/*.ts').reduce(function(obj, el) { 
+        obj[path.parse(el).name] = el;
+        return obj;
+    }, {}),
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            // All .ts and .tsx files should be handled by ts-loader
+            {test: /\.tsx?$/, loader: "ts-loader"}
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{
+                from: "**",
+                to: path.resolve(__dirname, "dist"),
+                context: path.resolve(__dirname, "src"),
+                globOptions: {
+                    ignore: ["**/*.ts"]
+                },
+                force: true,
+            }]
+        })
+    ]
+}
